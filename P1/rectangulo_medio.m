6448cc42ab7e93238982088c2a3f6d7fdc60d401
jp.galvez.2025 <jp.galvez.2025@VD018028>
2025-11-03 20:45:46 +0100

    fix(P1): inversed rectangulo_medio

--- P1/rectangulo_medio.m
+++ P1/rectangulo_medio.m
@@ -2,6 +2,6 @@ function [approximateIntegral] = rectangulo_medio(f,a,b)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-approximateIntegral = f((a + b) / 2) * (a - b)
+approximateIntegral = f((a + b) / 2) * (b - a)
 
 end
